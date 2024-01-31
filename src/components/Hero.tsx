d9325a34f88f58703e365901005091ad07970797
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-01-31 04:09:12 +0300

    add animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactTyped } from "react-typed";
 
 function Hero() {
   return (
@@ -10,11 +11,22 @@ function Hero() {
         <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl md:py-6">
           Grow with data.
         </h1>
-        <div>
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold">
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
             Fast, flexible financing for
           </p>
+          <ReactTyped
+            className="md:text-5xl md:pl-4 sm:text-4xl text-xl font-bold pl-2"
+            strings={["BTB", "BTC", "SAAS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to increase revenue for BTB, BTC, & SASS
+          platforms.
+        </p>
       </div>
     </div>
   );
