002a82fd5334a0ae4c1b22ae3c6fc215ebaf6bc3
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-01-31 03:43:47 +0300

    add hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState<boolean>(false);
 
   const handleNav = () => {
     setNav(!nav);
