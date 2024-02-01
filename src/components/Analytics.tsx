ccb5878623706a30998f114fa292d163f9abb57c
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-02-01 07:29:27 +0300

    add button

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import Laptop from "../assets/laptop.jpg";
 
 function Analytics() {
   return (
     <div className="w-full bg-white py-16 px-4">
-      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2"></div>
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img className="w-[500px] mx-auto my-4" src={Laptop} alt="laptop" />
+        <div className="flex flex-col justify-center">
+          <p className="text-[#00df9a] font-bold">DATA ANALYTICS DASHBOARD</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+            Manage Data Analytics Centrally
+          </h1>
+          <p className="">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+            consequuntur error ullam libero, porro ea? Temporibus, quo placeat
+            harum cum laborum aspernatur, quis a consequatur voluptatibus
+            dignissimos, velit maiores expedita.
+          </p>
+          <button className="w-[200px] rounded-md font-medium my-6 md:mx-0 py-3 text-[#00df9a] bg-black">
+            Get Started
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
