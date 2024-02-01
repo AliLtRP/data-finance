36350c25249e8ae1a381dea7d398326fd4fd733a
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2024-02-01 08:18:53 +0300

    style newsletter compnent

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 function Newsletter() {
-  return <div>Newsletter</div>;
+  return (
+    <div className="w-full py-16 text-white px-4">
+      <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
+        <div className="lg:col-span-2">
+          <h1 className="text-2xl md:text-4xl sm:text-3xl font-bold py-3">
+            Want tips & tricks to optimize your flow?
+          </h1>
+          <p>Sign up to our newsletter and stay up to date.</p>
+        </div>
+
+        <div className="my-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between w-full">
+            <input
+              className="p-3 flex w-full rounded-md text-black"
+              placeholder="Enter Email"
+              type="text"
+              name=""
+              id=""
+            />
+            <button className="w-[200px] rounded-md font-medium text-black bg-[#00df9a] ml-4 px-6 py-3">
+              Notify Me
+            </button>
+          </div>
+
+          <p>
+            We care bout the protection of your data. Read our{" "}
+            <span className="text-[#00df9a]">Privacy Policy.</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Newsletter;
